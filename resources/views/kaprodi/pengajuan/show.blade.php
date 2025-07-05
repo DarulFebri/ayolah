6438939d0fa5de9c49ffe2d2f969303801295dc7
Darul Febri <darulfer097@gmail.com>
2025-07-05 10:28:42 -0400

    ubah label dospem

--- resources/views/kaprodi/pengajuan/show.blade.php
+++ resources/views/kaprodi/pengajuan/show.blade.php
@@ -279,9 +279,9 @@
                         return sprintf('%s (<span class="status-badge %s">%s</span>)', e($dosen->nama), $statusClass, e(ucfirst($persetujuan)));
                     };
                 @endphp
-                <p><strong>Dosen Pembimbing:</strong> {!! $getDosenInfo(optional($pengajuan->sidang)->dosenPembimbing, optional($pengajuan->sidang)->persetujuan_dosen_pembimbing) !!}</p>
+                <p><strong>Dosen Pembimbing 1:</strong> {!! $getDosenInfo(optional($pengajuan->sidang)->dosenPembimbing, optional($pengajuan->sidang)->persetujuan_dosen_pembimbing) !!}</p>
                 @if (!$isPkl)
-                    <p><strong>Dosen Penguji 1:</strong> {!! $getDosenInfo(optional($pengajuan->sidang)->dosenPenguji1, optional($pengajuan->sidang)->persetujuan_dosen_penguji1) !!}</p>
+                    <p><strong>Dosen Pembimbing 2:</strong> {!! $getDosenInfo(optional($pengajuan->sidang)->dosenPenguji1, optional($pengajuan->sidang)->persetujuan_dosen_penguji1) !!}</p>
                 @endif
                 <p><strong>Ketua Sidang:</strong> {!! $getDosenInfo(optional($pengajuan->sidang)->ketuaSidang, optional($pengajuan->sidang)->persetujuan_ketua_sidang) !!}</p>
                 <p><strong>Sekretaris Sidang:</strong> {!! $getDosenInfo(optional($pengajuan->sidang)->sekretarisSidang, optional($pengajuan->sidang)->persetujuan_sekretaris_sidang) !!}</p>
