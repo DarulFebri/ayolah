1c5dc1af2516d1f2e2027aa98544891750749b34
Darul Febri <darulfer097@gmail.com>
2025-07-13 01:38:16 +0800

    fix : pada halaman /kaprodi/dashboard, hilangkan fitur manajemen dosen

--- resources/views/kaprodi/dashboard.blade.php
+++ resources/views/kaprodi/dashboard.blade.php
@@ -13,28 +13,6 @@
         <p>Sistem Informasi Praktek Kerja Lapangan dan Tugas Akhir - Politeknik Negeri Padang</p>
     </div>
     
-    <div class="stats-grid">
-        <div class="stats-card" style="animation-delay: 0.1s;">
-            <div class="stats-icon icon-blue">
-                <i class="fas fa-chalkboard-teacher"></i>
-            </div>
-            <div class="stats-content">
-                <h3>{{ $jumlahDosen }}</h3>
-                <p>Total Dosen</p>
-            </div>
-        </div>
-        
-        <div class="stats-card" style="animation-delay: 0.2s;">
-            <div class="stats-icon icon-green">
-                <i class="fas fa-file-contract"></i>
-            </div>
-            <div class="stats-content">
-                <h3>{{ $jumlahPengajuan }}</h3>
-                <p>Total Pengajuan Baru</p>
-            </div>
-        </div>
-    </div>
-    
     <div class="card-container">
         <a href="{{ route('kaprodi.pengajuan.index') }}" class="card-link">
             <div class="card clickable-card medium">
@@ -43,17 +21,6 @@
                 </div>
                 <h3 class="card-title">
                     Manajemen Pengajuan Sidang
-                </h3>
-            </div>
-        </a>
-        
-        <a href="{{ route('kaprodi.dosen.index') }}" class="card-link">
-            <div class="card clickable-card medium">
-                <div class="card-icon">
-                    <i class="fas fa-user-tie"></i>
-                </div>
-                <h3 class="card-title">
-                    Manajemen Dosen
                 </h3>
             </div>
         </a>
