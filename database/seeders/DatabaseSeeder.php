e7d137de11eb6ded67918d245492eb449f4bb46d
Darul Febri <darulfer097@gmail.com>
2025-07-21 09:31:32 +0800

    gatau

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             DosenSeeder::class,     // Ketiga, buat detail dosen
             KelasSeeder::class,     // Tambahkan ini untuk mengisi data kelas
             MahasiswaSeeder::class, // Keempat, buat detail mahasiswa
+            KaprodiSeeder::class,
             // Tambahkan seeder lain jika ada
         ]);
     }
